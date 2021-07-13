4d78928dd8371b82d2e50d295769972e83f0aa47
Bunz <66331265+66Bunz@users.noreply.github.com>
2021-07-13 17:44:20 +0000

    Added the possibility to add one photo to a suggestion

--- cogs/cog_sugg.py
+++ cogs/cog_sugg.py
@@ -14,32 +14,79 @@ collection_s = db["Suggestions"]
 
 
 
+
+
 class SuggestionCommands(commands.Cog, name="Suggestions"):
 
 
 
     def __init__(self, bot):
         self.bot = bot
+	
 
+
+	
 
 
     @commands.command(name='suggest', aliases=['sugg','sug'])
-    async def suggest(self, ctx,* , sugg):
+    async def suggest(self, ctx, *, sugg):
         """suggestion command"""
+        
         channel = self.bot.get_channel(self.bot.suggchannel)
-        embed = discord.Embed(title='New Suggestion By {}'.format(ctx.author.display_name), color=discord.Color.from_rgb(217, 144, 40))
-        embed.add_field(name='Suggestion: ', value=sugg)
-        embed.set_footer(text='UserID: ( {} ) | sID: ( {} )'.format(ctx.author.id, ctx.author.display_name), icon_url=ctx.author.avatar_url)
-        post={"UserID":ctx.author.id,"sID":ctx.author.display_name,"Suggestion":sugg}
-        collection_s.insert_one(post)
-        suggg = await channel.send(embed=embed)
-        await suggg.add_reaction("👍")
-        await suggg.add_reaction("👎")
-        embed = discord.Embed(description='☑️ Your Suggestion Has Been Sent To <#{}> !\nRemember to upvote it!'.format(channel.id))
-        await ctx.message.delete()
-        embsend = await ctx.send(embed = embed)
-        time.sleep(10)
-        await embsend.delete()
+        
+        
+        
+
+        
+        if ctx.message.attachments:
+        	
+        	attachment_url = ctx.message.attachments[0].url
+        	
+        	embed1 = discord.Embed(title='New Suggestion By {}'.format(ctx.author.display_name), color=discord.Color.from_rgb(217, 144, 40))
+        	
+        	
+        	embed2 = discord.Embed(color=discord.Color.from_rgb(217, 144, 40))
+        	embed2.add_field(name='Suggestion: ', value=sugg)
+        	embed2.set_footer(text='UserID: ( {} ) | sID: ( {} )'.format(ctx.author.id, ctx.author.display_name), icon_url=ctx.author.avatar_url)
+        	
+        	
+        	post={"UserID":ctx.author.id,"sID":ctx.author.display_name,"Suggestion":sugg,"Attachment":attachment_url}
+        	
+        	sugg1 = await channel.send(embed=embed1)
+        	attc = await channel.send(attachment_url)
+        	sugg2 = await channel.send(embed=embed2)
+            
+        	collection_s.insert_one(post)
+        	
+        	await sugg2.add_reaction("👍")
+        	await sugg2.add_reaction("👎")
+        	
+        	embed = discord.Embed(description='☑️ Your Suggestion Has Been Sent To <#{}> !'.format(channel.id))
+        	await ctx.message.delete()
+        	embsend = await ctx.send(embed = embed)
+        	time.sleep(5)
+        	await embsend.delete()
+        	
+        	
+        else:
+        	
+        	embed = discord.Embed(title=' prova New Suggestion By {}'.format(ctx.author.display_name), color=discord.Color.from_rgb(217, 144, 40))
+        	embed.add_field(name='Suggestion: ', value=sugg)
+        	embed.set_footer(text='UserID: ( {} ) | sID: ( {} )'.format(ctx.author.id, ctx.author.display_name), icon_url=ctx.author.avatar_url)
+        	
+        	post={"UserID":ctx.author.id,"sID":ctx.author.display_name,"Suggestion":sugg}
+        	
+        	suggg = await channel.send(embed=embed)
+
+        	
+        	collection_s.insert_one(post)
+        	await suggg.add_reaction("👍")
+        	await suggg.add_reaction("👎")
+        	embed = discord.Embed(description='☑️ Your Suggestion Has Been Sent To <#{}> !'.format(channel.id))
+        	await ctx.message.delete()
+        	embsend = await ctx.send(embed = embed)
+        	time.sleep(5)
+        	await embsend.delete
 
 
     @suggest.error
