1ac0a5acaf9b3ea8813f38d49db54478b35a9f91
Bunz <66331265+2Bunzen2@users.noreply.github.com>
2021-09-09 22:12:39 +0200

    Version 2.6 — Code styling

--- keep_alive.py
+++ keep_alive.py
@@ -19,8 +19,7 @@ def run():
 
 
 def keep_alive():
-    '''
-    Creates and starts new thread that runs the function run.
-    '''
+    '''Creates and starts new thread that runs the function run.'''
+    
     t = Thread(target=run)
     t.start()
