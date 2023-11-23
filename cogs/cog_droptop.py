8865eba81ee9725d366dd3896d4d52739e67446e
66Bunz <66bunz@gmail.com>
2023-11-23 21:08:14 +0000

    Fixed interaction expiration when calling new_release modal

--- cogs/cog_droptop.py
+++ cogs/cog_droptop.py
@@ -381,7 +381,7 @@ class DroptopCommands(commands.Cog):
 		else:
 			await interaction.response.send_message(f"The {name} community app doesn't exists.", ephemeral=True)
 		
-
+	
 	@community_app_group.command(name="new_release")
 	@app_commands.describe(
 		rmskin_package="The package of your Community App"
@@ -391,12 +391,65 @@ class DroptopCommands(commands.Cog):
 		
 		channel = self.bot.get_channel(self.bot.configs["appreleases_channel"])
 
+		await interaction.response.send_message("Checking if the package is valid...", ephemeral=True)
+
 		if rmskin_name_check("app", rmskin_package.filename):
 			rmskin_name = rmskin_rename("app", rmskin_package.filename)
 			rmskin_path = Path(f"tmp/{rmskin_name}")
 			await rmskin_package.save(rmskin_path)
 
-			await interaction.response.send_modal(NewAppRelease(self.bot.configs, rmskin_path, rmskin_name, channel))
+			with zipfile.ZipFile(rmskin_path, "r") as rmskin_archive:
+				ini_path = Path("tmp/RMSKIN.ini")
+				rmskin_archive.extract("RMSKIN.ini", "tmp")
+				config = configparser.ConfigParser()
+				config.read(ini_path)
+				app_title = config['rmskin']['Name']
+				author = config['rmskin']['Author']
+				version = config['rmskin']['Version']
+				try: 
+					UUID = config['rmskin']['GUID']
+				except:
+					await interaction.followup.send("No UUID was found inside your rmskin package!\nBe sure to update droptop and repackage your app.", ephemeral=True)
+					return
+
+				ini_path.unlink()
+
+				preview_image = f"Skins\Droptop Community Apps\Apps\{app_title.replace(' ', '_')}-{author.replace(' ', '_')}\Images\PreviewImage.png"
+				if preview_image in rmskin_archive.namelist():
+					pass
+				else:
+					await interaction.followup.send("No image preview was found inside your rmskin package!\nBe sure to insert an image during the packaging process and repackage your app.", ephemeral=True)
+					return
+
+			data = github_reader(self.bot.configs["github_private_key"], "data/community_apps/community_apps.json")
+			for app in data["apps"]:
+				if UUID == app["app"]["uuid"]:
+					new = False
+					default_description = app["app"]["desc"]
+					default_github_profile = app["app"]["author_link"]
+					default_github_repo = app["app"]["official_link"]
+					break
+				else:
+					new = True
+					default_description = ""
+					default_github_profile = ""
+					default_github_repo = ""
+
+			async def confirm_callback(interaction):
+				await interaction.response.send_modal(NewAppRelease(self.bot.configs, rmskin_path, rmskin_name, channel, new, default_description, default_github_profile, default_github_repo))
+
+			async def cancel_callback(interaction):
+				await interaction.response.send_message("Action cancelled...")
+
+			confirm_button = discord.ui.Button(label="Confirm" , style=discord.ButtonStyle.green)
+			cancel_button = discord.ui.Button(label="Cancel", style=discord.ButtonStyle.red)
+			confirm_button.callback = confirm_callback
+			cancel_button.callback = cancel_callback
+			view = discord.ui.View()
+			view.add_item(confirm_button)
+			view.add_item(cancel_button)
+			original = await interaction.original_response()
+			await original.edit(content=f"Your package is fine!\nDo you want to continue with the release of the **{app_title}** by **{author}** *(v{version})*?", view=view)
 
 		else:
 			await interaction.response.send_message("No rmskin app package was found, be sure to put it in the right hitbox the next time.", ephemeral=True)
@@ -440,7 +493,7 @@ class DroptopCommands(commands.Cog):
 	async def community_app_delete(self, interaction: discord.Interaction, community_app: str, delete_release_channel: Optional[str] = "True"):
 		"""Deletes a Community App Release."""
 
-		view = Confirm(community_app)
+		view = ConfirmDeletion(community_app)
 		await interaction.response.send_message(f"Are you sure you want to permanently delete:\n```diff\n- {community_app}\n```\n*(This action is irreversible)*", ephemeral=True, view=view)
 		await view.wait()
 		if view.value is None:
@@ -617,7 +670,7 @@ class DroptopCommands(commands.Cog):
 	async def community_theme_delete(self, interaction: discord.Interaction, community_theme: str, delete_release_channel: Optional[str] = "True"):
 		"""Deletes a Community Theme Release."""
 
-		view = Confirm(community_theme)
+		view = ConfirmDeletion(community_theme)
 		await interaction.response.send_message(f"Are you sure you want to permanently delete:\n```diff\n- {community_theme}\n```\n*(This action is irreversible)*", ephemeral=True, view=view)
 		await view.wait()
 		if view.value is None:
@@ -651,38 +704,16 @@ class DroptopCommands(commands.Cog):
 
 
 class NewAppRelease(discord.ui.Modal, title="New App Release"):
-	def __init__(self, configs, rmskin_path, rmskin_name, channel):
+	def __init__(self, configs, rmskin_path, rmskin_name, channel, new, default_description, default_github_profile, default_github_repo):
 		super().__init__()
 		self.configs = configs
 		self.rmskin_path = rmskin_path
 		self.rmskin_name = rmskin_name
 		self.channel = channel
-
-		with zipfile.ZipFile(self.rmskin_path, "r") as rmskin_archive:
-			ini_path = Path("tmp/RMSKIN.ini")
-			rmskin_archive.extract("RMSKIN.ini", "tmp")
-			config = configparser.ConfigParser()
-			config.read(ini_path)
-			self.app_title = config['rmskin']['Name']
-			self.author = config['rmskin']['Author']
-			self.version = config['rmskin']['Version']
-			self.UUID = config['rmskin']['GUID']
-	
-		ini_path.unlink()
-	
-		data = github_reader(self.configs["github_private_key"], "data/community_apps/community_apps.json")
-		for app in data["apps"]:
-			if self.UUID == app["app"]["uuid"]:
-				#self.uuid = app["app"]["uuid"]
-				self.default_description = app["app"]["desc"]
-				self.default_github_profile = app["app"]["author_link"]
-				self.default_github_repo = app["app"]["official_link"]
-				break
-			else:
-				#self.uuid = ""
-				self.default_description = ""
-				self.default_github_profile = ""
-				self.default_github_repo = ""
+		self.new = new
+		self.default_description = default_description
+		self.default_github_profile = default_github_profile
+		self.default_github_repo = default_github_repo
 	
 		self.description = discord.ui.TextInput(
 			label="Description",
@@ -718,41 +749,43 @@ class NewAppRelease(discord.ui.Modal, title="New App Release"):
 
 	async def on_submit(self, interaction: discord.Interaction):
 
+		await interaction.response.send_message("Your app is being released... Please wait...", ephemeral=True)
+		original = await interaction.original_response()
 		community_json = github_reader(self.configs["github_private_key"], "data/community_apps/community_apps.json")
 		authorised_members = []
-		await interaction.response.send_message("Your app is being released... Please wait...", ephemeral=True)
 
 		with zipfile.ZipFile(self.rmskin_path, "r") as rmskin_archive:
+			ini_path = Path("tmp/RMSKIN.ini")
+			rmskin_archive.extract("RMSKIN.ini", "tmp")
+			config = configparser.ConfigParser()
+			config.read(ini_path)
+			app_title = config['rmskin']['Name']
+			author = config['rmskin']['Author']
+			version = config['rmskin']['Version']
+			UUID = config['rmskin']['GUID']
 
-			preview_image = f"Skins\Droptop Community Apps\Apps\{self.app_title.replace(' ', '_')}-{self.author.replace(' ', '_')}\Images\PreviewImage.png"
+			ini_path.unlink()
 				
-			if preview_image in rmskin_archive.namelist():
-				image_name = f"{self.app_title}-{self.author}"
-				image_name = img_rename("app", image_name)
-				image_path = Path(f"tmp/{image_name}.png")
-				rmskin_archive.extract(preview_image, "tmp")
-				os.rename(f"tmp/{preview_image}", image_path)
-				webp_path = to_webp(image_path)
-			else:
-				await interaction.followup.send("No image preview was found inside your rmskin package!\nBe sure to update droptop (>v4.330.38) and repackage your app.", ephemeral=True)
-				return
+			preview_image = f"Skins\Droptop Community Apps\Apps\{app_title.replace(' ', '_')}-{author.replace(' ', '_')}\Images\PreviewImage.png"
+			image_name = f"{app_title}-{author}"
+			image_path = Path(f"tmp/{image_name}.png")
+			rmskin_archive.extract(preview_image, "tmp")
+			os.rename(f"tmp/{preview_image}", image_path)
+			webp_path = to_webp(image_path)
 
 		for item in community_json["apps"]:
 			app_tags = item["app"]
-			if app_tags["name"] == self.app_title:
-				new = False
+			if app_tags["uuid"] == UUID:
 				for member in app_tags["authorised_members"]:
 					authorised_members.append(member)
-			else:
-				new = True
 
-		if new:
+		if self.new:
 			authorised_members = [self.configs["author_id"], self.configs["cari_id"], interaction.user.id]
 		
 		if interaction.user.id in authorised_members:
 			rmskin_creation = push_rmskin(self.configs["github_private_key"], "app", self.rmskin_name)
 			image_creation = push_image(self.configs["github_private_key"], "app", image_name)
-			updated_json, download_link, image_link, app_id, uuid = json_update(self.configs["github_private_key"], "app", authorised_members=authorised_members, title=self.app_title, author=self.author, description=self.description.value, changenotes= self.changenotes.value, rmskin_name=self.rmskin_name, image_name=image_name, version=self.version, uuid=self.UUID, author_link=self.github_profile.value, github_repo=self.github_repo.value)
+			updated_json, download_link, image_link, app_id, uuid = json_update(self.configs["github_private_key"], "app", authorised_members=authorised_members, title=self.app_title, author=self.author, description=self.description.value, changenotes= self.changenotes.value, rmskin_name=self.rmskin_name, image_name=image_name, version=self.version, uuid=UUID, author_link=self.github_profile.value, github_repo=self.github_repo.value)
 
 			view = discord.ui.View()
 			style = discord.ButtonStyle.url
@@ -760,10 +793,11 @@ class NewAppRelease(discord.ui.Modal, title="New App Release"):
 			site_button = discord.ui.Button(style=style, label="See on Website", url=f"https://droptopfour.com/community-apps?id={app_id}")
 			view.add_item(item=download_button)
 			view.add_item(item=site_button)
-			embed = discord.Embed(title=f"{self.app_title} - {self.author}", description=f"{self.description.value}", color=discord.Color.from_rgb(75, 215, 100))
+			embed = discord.Embed(title=f"{app_title} - {author}", description=f"{self.description.value}", color=discord.Color.from_rgb(75, 215, 100))
 			embed.set_author(name="New Community App Release", url=self.configs["website"]+"/community-apps")
-			embed.add_field(name="Version: ", value=self.version, inline=False)
-			embed.set_footer(text=f"UserID: ( {interaction.user.id} ) | uuid: ( {self.UUID} )", icon_url=interaction.user.avatar.url)
+			embed.add_field(name="Version: ", value=version, inline=False)
+			embed.add_field(name="Changenotes:", value=self.changenotes.value, inline=False)
+			embed.set_footer(text=f"UserID: ( {interaction.user.id} ) | uuid: ( {UUID} )", icon_url=interaction.user.avatar.url)
 			embed.set_image(url=image_link)
 			all_threads = []
 			for thread in self.channel.threads:
@@ -773,16 +807,18 @@ class NewAppRelease(discord.ui.Modal, title="New App Release"):
 				all_threads.append(thread)
 
 			for thread in all_threads:
-				if thread.name == f"{self.app_title} - {self.author}":
+				if thread.name == f"{app_title} - {author}":
 					await thread.send(embed=embed, view=view)
 					break
 			else:
-				await self.channel.create_thread(name=f"{self.app_title} - {self.author}", embed=embed, view=view)
+				await self.channel.create_thread(name=f"{app_title} - {author}", embed=embed, view=view)
+
 			webp_path.unlink()
-			await interaction.followup.send(f"You successfully published **{self.app_title}** in <#{self.channel.id}>", ephemeral=True)
+			await original.edit(content=f"You successfully published **{app_title}** in <#{self.channel.id}>")
 			self.rmskin_path.unlink()
+
 		else:
-			await interaction.response.send_message(f"You aren't authorised to publish updates, modify or delete {self.app_title}.\nAsk {self.app_author} to add you as an authorised user.", ephemeral=True)
+			await interaction.response.send_message(f"You aren't authorised to publish updates, modify or delete {app_title}.\nAsk {author} to add you as an authorised user.", ephemeral=True)
 			
 	async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
 		await interaction.followup.send(f"Oops! Something went wrong, contact Bunz.\n{error}", ephemeral=True)
@@ -1237,7 +1273,7 @@ class EditThemeRelease(discord.ui.Modal, title="Edit Theme Release"):
 		traceback.print_tb(error.__traceback__)
 
 
-class Confirm(discord.ui.View):
+class ConfirmDeletion(discord.ui.View):
 	def __init__(self, community_app):
 		super().__init__()
 		self.community_app = community_app
@@ -1247,14 +1283,14 @@ class Confirm(discord.ui.View):
 	@discord.ui.button(label='Confirm', style=discord.ButtonStyle.red)
 	async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
 		self.value = True
-		await interaction.response.send_message(f"The `{self.community_app}` app is being deleted...", ephemeral=True)
+		await interaction.response.send_message(f"The `{self.community_app}` package is being deleted...", ephemeral=True)
 		self.stop()
 
 
 	@discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
 	async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
 		self.value = False
-		await interaction.response.send_message(f"The `{self.community_app}` app **will NOT** be deleted.", ephemeral=True)
+		await interaction.response.send_message(f"The `{self.community_app}` package **will NOT** be deleted.", ephemeral=True)
 		self.stop()
 
 
